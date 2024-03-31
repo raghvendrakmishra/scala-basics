56daf5e92489979963d8fbc2117eb00505352964
rmishra <raghvendra.k.mishra@outlook.com>
2024-03-31 09:58:03 +0530

    Understanding call by value and call by name

---
+++ src/main/scala/lectures/basics/CBNvsCBV.scala
@@ -0,0 +1,23 @@
+package lectures.basics
+
+object CBNvsCBV extends App {
+
+  private def calledByValue(x: Long): Unit = {
+    println("call by value " + x)
+    println("call by value " + x)
+  }
+
+  private def calledByName(x: => Long): Unit = {
+    println("call by name " + x)
+    println("call by name " + x)
+  }
+
+  calledByValue(System.nanoTime())
+  calledByName(System.nanoTime())
+
+  private def infinite(): Int = 1 + infinite()
+  private def printFirst(x: Int, y: => Int) = x
+
+  // println(printFirst(infinite(), 34)) // stackoverflow, as first value evaluated
+  println(printFirst(34, infinite())) // dont' crash as second value infinite() is never invoked, hence not executed
+}
