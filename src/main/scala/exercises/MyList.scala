549f1f023580b00749a50950bada7537eeb35151
rmishra <raghvendra.k.mishra@outlook.com>
2024-06-22 10:59:25 +0530

    OO exercise. Extends collection

--- src/main/scala/exercises/MyList.scala
+++ src/main/scala/exercises/MyList.scala
@@ -1,5 +1,6 @@
 package exercises
 
+import scala.annotation.targetName
 import scala.reflect.ClassManifestFactory.{Any, Nothing, Null}
 
 /**
@@ -20,6 +21,12 @@ abstract class MyList[+A] {
   def add[B >: A](element: B): MyList[B]
   def printElement: String
   override def toString: String = s"[ $printElement ]"
+
+  def map[B](transformer: MyTransformer[A, B]): MyList[B]
+  def flatMap[B](transformer: MyTransformer[A, MyList[B]]): MyList[B]
+  def filter(predicate: MyPredicate[A]): MyList[A]
+
+  def ++[B >: A](list: MyList[B]): MyList[B]
 }
 
 object MyEmptyList extends MyList[Nothing] {
@@ -28,6 +35,12 @@ object MyEmptyList extends MyList[Nothing] {
   def isEmpty: Boolean = true
   def add[B >: Nothing](element: B): MyList[B] = new MyNonEmptyList(element, MyEmptyList)
   def printElement: String = ""
+
+  def map[B](transformer: MyTransformer[Nothing, B]): MyList[B] = MyEmptyList
+  def flatMap[B](transformer: MyTransformer[Nothing, MyList[B]]): MyList[B] = MyEmptyList
+  def filter(predicate: MyPredicate[Nothing]): MyList[Nothing] = MyEmptyList
+
+  def ++[B >: Nothing](list: MyList[B]): MyList[B] = list
 }
 
 class MyNonEmptyList[+A](h: A, t: MyList[A]) extends MyList[A] {
@@ -36,6 +49,57 @@ class MyNonEmptyList[+A](h: A, t: MyList[A]) extends MyList[A] {
   def isEmpty: Boolean = false
   def add[B >: A](element: B): MyList[B] = new MyNonEmptyList(element, t)
   def printElement: String = if(t.isEmpty) head.toString else s"$head, ${t.printElement}"
+
+  /**
+   * [1,2,3].filter(n % 2 == 0)
+   * = [2,3].filter(n % 2 == 0)
+   * = new MyNonEmptyList(2, [3].filter(n % 2 == 0))
+   * = new MyNonEmptyList(2, MyEmptyList.filter(n % 2 == 0))
+   *  = new MyNonEmptyList(2, MyEmptyList)
+   */
+  def filter(predicate: MyPredicate[A]): MyList[A] = {
+    if (predicate.test(h)) new MyNonEmptyList(h, t.filter(predicate))
+    else t.filter(predicate)
+  }
+
+  /**
+   * [1,2,3].map(n * 2)
+   * = new MyNonEmptyList(2, [2,3).map(n * 2)
+   * = new MyNonEmptyList(2, new MyNonEmptyList(4, [3].map(n * 2)]
+   * = new MyNonEmptyList(2, new MyNonEmptyList(4, new MyNonEmptyList(6, MyEmptyList.map(n * 2)
+   */
+  def map[B](transformer: MyTransformer[A, B]): MyList[B] = new MyNonEmptyList(transformer.transform(h), t.map(transformer))
+
+  /**
+   * [1,2] ++ [3,4,5]
+   * = new MyNonEmptyList(1, [2] ++ [3,4,5])
+   * = new MyNonEmptyList(1, new MyNonEmptyList(2, Empty ++ [3,4,5])
+   * = new MyNonEmptyList(1, new MyNonEmptyList(2, [3,4,5])
+   */
+  def ++[B >: A](list: MyList[B]): MyList[B] = new MyNonEmptyList(h, t ++ list)
+  /**
+   * [1,2].flatMap(n => (n, n+1))
+   *  = [1,2] ++ [2].flatMap(n => (n, n+1))
+   *  = [1,2] ++ [2,3] ++ MyEmptyList.flatMap(n => (n, n+1))
+   *  = [1,2) ++ [2,3]
+   */
+  def flatMap[B](transformer: MyTransformer[A, MyList[B]]): MyList[B] = transformer.transform(h) ++ t.flatMap(transformer)
+}
+
+trait MyPredicate[-T] {
+  def test(t: T): Boolean
+}
+
+trait MyTransformer[-A, B] {
+  def transform(a: A): B
+}
+
+object EvenPredicate extends MyPredicate[Int] {
+  def test(t: Int): Boolean = t % 2 == 0
+}
+
+object StringToIntTransformer extends MyTransformer[String, Int] {
+  def transform(a: String): Int = Integer.parseInt(a)
 }
 
 // Testing the list
@@ -53,4 +117,18 @@ object ListTest extends App {
   println(listOfIntegers.toString)
   println(listOfStrings.toString)
 
+  println(listOfIntegers.map(new MyTransformer[Int, Int] {
+    override def transform(a: Int): Int = a * 2
+  }).toString)
+  println(listOfIntegers.filter(new MyPredicate[Int] {
+    override def test(a: Int): Boolean = a % 2 == 0
+  }).toString)
+  val anotherListOfIntegers = new MyNonEmptyList(4, new MyNonEmptyList(5, MyEmptyList))
+  println((listOfIntegers ++ anotherListOfIntegers).toString)
+
+  println(anotherListOfIntegers.flatMap(
+    new MyTransformer[Int, MyList[Int]] {
+      override def transform(a: Int): MyList[Int] = new MyNonEmptyList(a, new MyNonEmptyList(a + 1, MyEmptyList))
+    }
+  ).toString)
 }
--- src/main/scala/lectures/part2oop/AnonymousClasses.scala
+++ src/main/scala/lectures/part2oop/AnonymousClasses.scala
@@ -19,11 +19,30 @@ object AnonymousClasses extends App {
    */
   hungryAnimal.eat
   println(hungryAnimal.getClass)
-  
+
   class Person(name: String) {
     def sayHi = s"$name is saying Hi..."
   }
   val swpanil = new Person("Swapnil") {
     override def sayHi = "Swapnil is saying Hello..."
   }
+
+  /**
+   * Exercise:
+   *  1. Generic trait MyPredicate[-T] // -A => contra-variant
+   *    test(T): Boolean
+   *  2. Generic trait MyTransformer[-A, B]
+   *    transform(A): B
+   *  3. MyList
+   *    map(MyTransformer): MyList
+   *    filter(MyPredicate): MyList
+   *    flatMap(MyTransformer from A to MyList[B]): MyList[B]
+   *  4. EvenPredicate extends MyPredicate[Int]
+   *  5. StringToIntTransformer extends MyTransformer[String, Int]
+   *
+   *  Examples:
+   *  [1,2,3].map(n * 2) = [2,4,6]
+   *  [1,2,3,4].filter(n % 2) = [2,4]
+   *  [1,2,3].flatMap(n => (n, n + 1 ) = [1, 2, 2, 3, 3, 4]
+   */
 }
